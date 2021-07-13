787f3ec2c3a246a782b63aeeb3f1b4cbc83c37d0
Cherokee Walters <cherokeewalters162@gmail.com>
2021-07-13 18:00:31 -0400

    accepts a file and is promise based

--- index.js
+++ index.js
@@ -13,42 +13,44 @@ Edge Cases
 Any value that isn't a string or an array of strings.
 
 */
+
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-let argv = process.argv;
-argv = argv.slice(2)
+filePath = process.argv[2];
 
-let url;
+if (filePath === undefined) {
+  console.log("Please enter a filepath/name");
+} else {
+  fs.promises.readFile(filePath, 'utf-8')
+  .then(data => data.split('\n'))
+  .then(data => pullPokemonTypes(data))
+  .catch(error => console.log("Please enter a correct file path and name."));
+};
+//pokemonNameList = pokemonNameList.split('\n');
 
-// fetch('https://pokeapi.co/api/v2/pokemon/charizard')
-//   .then(response => response.json())
-//   .then((pokemon) => {
-//     let pokemonTypes = ''
-//     for(let i = 0; i < pokemon.types.length; i++){
-//       pokemonTypes += pokemon.types[i].type.name + ' ';
-//     }
-//     pokemonTypes = pokemonTypes.split(' ').join(', ').slice(0, pokemonTypes.length - 0) + '.';
-//     console.log(pokemon.name + ': ' + pokemonTypes);
-//   })
-//   .catch(error => console.log(error));
-
-for (let i = 0; i < argv.length; i++) {
-  let url = 'https://pokeapi.co/api/v2/pokemon/' + argv[i];
-  fetch(url)
-  .then(response => response.json())
-  .then((pokemon) => {
-    let pokemonTypes = ''
-    let numberOfTypes = 0;
-    for(let i = 0; i < pokemon.types.length; i++){
-      pokemonTypes += pokemon.types[i].type.name + ' ';
-      numberOfTypes += 1;
+function pullPokemonTypes (list) {
+  return new Promise ((resolve, reject) => {
+    for (let i = 0; i < list.length; i++) {
+      let url = 'https://pokeapi.co/api/v2/pokemon/' + list[i];
+      fetch(url)
+      .then(response => response.json())
+      .then((pokemon) => {
+        let pokemonTypes = '';
+        let numberOfTypes = 0;
+        for(let i = 0; i < pokemon.types.length; i++){
+          pokemonTypes += pokemon.types[i].type.name + ' ';
+          numberOfTypes += 1;
+        }
+        if (numberOfTypes > 1) {
+          pokemonTypes = pokemonTypes.split(' ').join(', ').slice(0, pokemonTypes.length) + '.';
+        } else {
+          pokemonTypes = pokemonTypes.slice(0, pokemonTypes.length - 1) + '.';
+        }
+        console.log(pokemon.name + ': ' + pokemonTypes);
+        resolve();
+      })
+      .catch(error => console.log(Error + ' is not a valid pokemon name'));
     }
-    if (numberOfTypes > 1) {
-      pokemonTypes = pokemonTypes.split(' ').join(', ').slice(0, pokemonTypes.length - 0) + '.';
-    } else {
-      pokemonTypes = pokemonTypes.slice(0, pokemonTypes.length - 1) + '.';
-    }
-    console.log(pokemon.name + ': ' + pokemonTypes);
   })
-  .catch(error => console.log('Please enter a valid pokemon name.'));
 }
